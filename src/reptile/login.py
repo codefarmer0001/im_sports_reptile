96be5e06253d48b3491d8cb01a848919bbd299de
codefarmer0001 <zjack4759@gmail.com>
2024-06-29 20:08:07 +0800

    优化参数

--- src/reptile/login.py
+++ src/reptile/login.py
@@ -42,7 +42,7 @@ class login:
         # 创建Chrome浏览器对象
         chrome_options = webdriver.ChromeOptions()
         chrome_options.add_argument('--headless')
-        # chrome_options.add_argument('--no-sandbox')
+        chrome_options.add_argument('--no-sandbox')
         # chrome_options.add_argument('--disable-dev-shm-usage')
         # chrome_options.add_argument('--disable-gpu')
         # chrome_options.add_argument('--disable-extensions')
